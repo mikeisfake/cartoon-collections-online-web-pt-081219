14474186b17ed68531bf1eb1123dac2cbcdaf58e
Mike Cooper <mikescooper@icloud.com>
2019-08-21 19:01:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(calls)
   end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.include?(cheese_types.)
 end
